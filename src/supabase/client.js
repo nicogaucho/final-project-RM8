1785d30a0e276f436f76eab0bd789efae87cb8d2
Nicola Gasparro <nicola.gasp11@gmail.com>
2023-12-11 11:49:12 +0000

    .env file

--- src/supabase/client.js
+++ src/supabase/client.js
@@ -1,11 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-// const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
 
-const supabaseUrl = 'https://ulovwvhtsiyhnioiqbdo.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsb3Z3dmh0c2l5aG5pb2lxYmRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA3NjM1MDAsImV4cCI6MjAxNjMzOTUwMH0.nXTQD-b6f4ilgishLa_vd66Uat-9E4vkCXcUsNtEhrg';
 const supabase = createClient(supabaseUrl, supabaseKey, {
   multiTab: false,
 });
